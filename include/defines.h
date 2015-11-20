53f57ecdc026b229123feaf4bd0a3e513f49042a
Hendrik v. Raven <hendrik@consetetur.de>
2015-11-20 12:56:11 +0000

    define default gateway

--- include/defines.h
+++ include/defines.h
@@ -19,6 +19,11 @@
 #define NETMASK_ADDR2 255
 #define NETMASK_ADDR3 0
 
+#define GW_ADDR0 172
+#define GW_ADDR1 31
+#define GW_ADDR2 10
+#define GW_ADDR3 1
+
 // chip is soldered in RMII_MODE not in MII_MODE
 #define RMII_MODE
 
