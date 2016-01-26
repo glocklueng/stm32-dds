0b5a3baf2844d16ade621e9bd34d7d234db65926
Hendrik v. Raven <hendrik@consetetur.de>
2016-01-26 16:50:29 +0000

    don't use tm library for spi
    inlined the code from the library and simplified it for this usage

--- include/spi.h
+++ include/spi.h
@@ -1,19 +1,42 @@
 #ifndef _SPI_H
 #define _SPI_H
 
-#include <tm_stm32f4_spi.h>
+#include <stm32f4xx_spi.h>
+
+/* our used SPI channel */
+#define SPIx SPI1
+
+/* helper macros taken from tm library */
+#define SPI_IS_BUSY(SPIy)                                                      \
+  (((SPIy)->SR & (SPI_SR_TXE | SPI_SR_RXNE)) == 0 || ((SPIy)->SR & SPI_SR_BSY))
+
+#define SPI_WAIT(SPIy) while (SPI_IS_BUSY(SPIy))
 
 void spi_init();
+
+inline uint8_t
+spi_send_single(uint8_t data)
+{
+  /* wait until previous transmission is complete */
+  SPI_WAIT(SPIx);
+
+  /* fill output buffer */
+  SPIx->DR = data;
+
+  SPI_WAIT(SPIx);
+
+  /* return received data */
+  return SPIx->DR;
+}
 
 inline void
 spi_write_single(uint8_t data)
 {
-  TM_SPI_Send(SPI1, data);
+  SPI_WAIT(SPIx);
+
+  SPIx->DR = data;
 }
 
-inline void
-spi_write_multi(uint8_t* data, uint32_t length)
-{
-  TM_SPI_WriteMulti(SPI1, data, length);
-}
+void spi_write_multi(uint8_t* data, uint32_t length);
+
 #endif /* _SPI_H */
--- src/spi.c
+++ src/spi.c
@@ -1,5 +1,8 @@
 #include "spi.h"
 
+#include <stm32f4xx_spi.h>
+#include <tm_stm32f4_gpio.h>
+
 /**
  * SPI communication with the AD9910. Uses the TM library to make
  * programming a bit simpler. For documentation on that see here:
@@ -9,6 +12,9 @@
  * serial programming interface of the DDS chip.
  */
 
+#define SPI_DATASIZE SPI_DataSize_8b;
+#define SPI_PRESCALER SPI_BaudRatePrescaler_32;
+
 /**
  * this functions sets up all the spi communication. To only usage of SPI
  * is currently the communication between AD9910 and the STM32F4
@@ -16,5 +22,50 @@
 void
 spi_init()
 {
-  TM_SPI_Init(SPI1, TM_SPI_PinsPack_2);
+  SPI_InitTypeDef spi_init;
+
+  SPI_StructInit(&spi_init);
+
+  /* enable SPI clock */
+  RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+
+  /* init pins */
+  TM_GPIO_InitAlternate(GPIOB, GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5,
+                        TM_GPIO_OType_PP, TM_GPIO_PuPd_NOPULL,
+                        TM_GPIO_Speed_High, GPIO_AF_SPI1);
+
+  /* set options */
+  spi_init.SPI_DataSize = SPI_DATASIZE;
+  spi_init.SPI_BaudRatePrescaler = SPI_PRESCALER;
+  spi_init.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+  spi_init.SPI_FirstBit = SPI_FirstBit_MSB;
+  spi_init.SPI_Mode = SPI_Mode_Master;
+  spi_init.SPI_NSS = SPI_NSS_Soft;
+  /* SPI mode */
+  spi_init.SPI_CPOL = SPI_CPOL_Low;
+  spi_init.SPI_CPHA = SPI_CPHA_1Edge;
+
+  /* disable first */
+  SPI1->CR1 &= ~SPI_CR1_SPE;
+
+  SPI_Init(SPI1, &spi_init);
+
+  /* enable SPI */
+  SPI1->CR1 |= SPI_CR1_SPE;
+}
+
+void
+spi_write_multi(uint8_t* data, uint32_t length)
+{
+  /* TODO let that use DMA */
+  SPI_WAIT(SPIx);
+
+  for (uint32_t i = 0; i < length; ++i) {
+    SPIx->DR = data[i];
+
+    SPI_WAIT(SPIx);
+
+    /* we need to read the data register */
+    (void)SPIx->DR;
+  }
 }
