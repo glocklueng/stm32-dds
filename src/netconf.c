82d624fa036c27e94b310d4e2b1e59f610be1a84
Hendrik v. Raven <hendrik@consetetur.de>
2016-01-26 17:36:44 +0000

    remove dhcp code from netconf

--- src/netconf.c
+++ src/netconf.c
@@ -71,10 +71,8 @@ LwIP_Init(void)
   struct ip_addr ipaddr;
   struct ip_addr netmask;
   struct ip_addr gw;
-#ifndef USE_DHCP
   uint8_t iptab[4] = { 0 };
   uint8_t iptxt[20];
-#endif
 
   /* Initializes the dynamic memory heap defined by MEM_SIZE.*/
   mem_init();
@@ -82,16 +80,10 @@ LwIP_Init(void)
   /* Initializes the memory pools defined by MEMP_NUM_x.*/
   memp_init();
 
-#ifdef USE_DHCP
-  ipaddr.addr = 0;
-  netmask.addr = 0;
-  gw.addr = 0;
-#else
   IP4_ADDR(&ipaddr, IP_ADDR0, IP_ADDR1, IP_ADDR2, IP_ADDR3);
   IP4_ADDR(&netmask, NETMASK_ADDR0, NETMASK_ADDR1, NETMASK_ADDR2,
            NETMASK_ADDR3);
   IP4_ADDR(&gw, GW_ADDR0, GW_ADDR1, GW_ADDR2, GW_ADDR3);
-#endif
 
   /* - netif_add(struct netif *netif, struct ip_addr *ipaddr,
   struct ip_addr *netmask, struct ip_addr *gw,
@@ -117,40 +109,9 @@ LwIP_Init(void)
 
     /* When the netif is fully configured this function must be called.*/
     netif_set_up(&gnetif);
-#ifdef USE_DHCP
-    DHCP_state = DHCP_START;
-#else
-#ifdef USE_LCD
-
-    iptab[0] = IP_ADDR3;
-    iptab[1] = IP_ADDR2;
-    iptab[2] = IP_ADDR1;
-    iptab[3] = IP_ADDR0;
-
-    sprintf((char*)iptxt, "  %d.%d.%d.%d", iptab[3], iptab[2], iptab[1],
-            iptab[0]);
-
-    LCD_DisplayStringLine(Line8, (uint8_t*)"  Static IP address   ");
-    LCD_DisplayStringLine(Line9, iptxt);
-#endif
-#endif /* USE_DHCP */
   } else {
     /*  When the netif link is down this function must be called.*/
     netif_set_down(&gnetif);
-#ifdef USE_DHCP
-    DHCP_state = DHCP_LINK_DOWN;
-#endif /* USE_DHCP */
-#ifdef USE_LCD
-    /* Set the LCD Text Color */
-    LCD_SetTextColor(Red);
-
-    /* Display message on the LCD */
-    LCD_DisplayStringLine(Line5, (uint8_t*)"  Network Cable is  ");
-    LCD_DisplayStringLine(Line6, (uint8_t*)"    not connected   ");
-
-    /* Set the LCD Text Color */
-    LCD_SetTextColor(White);
-#endif /* USE_LCD */
   }
 
   /* Set the link callback function, this function is called on change of link
@@ -193,129 +154,6 @@ LwIP_Periodic_Handle(__IO uint32_t localtime)
     etharp_tmr();
   }
 
-#ifdef USE_DHCP
-  /* Fine DHCP periodic process every 500ms */
-  if (localtime - DHCPfineTimer >= DHCP_FINE_TIMER_MSECS) {
-    DHCPfineTimer = localtime;
-    dhcp_fine_tmr();
-    if ((DHCP_state != DHCP_ADDRESS_ASSIGNED) && (DHCP_state != DHCP_TIMEOUT) &&
-        (DHCP_state != DHCP_LINK_DOWN)) {
-      /* toggle LED1 to indicate DHCP on-going process */
-      STM_EVAL_LEDToggle(LED1);
-
-      /* process DHCP state machine */
-      LwIP_DHCP_Process_Handle();
-    }
-  }
-
-  /* DHCP Coarse periodic process every 60s */
-  if (localtime - DHCPcoarseTimer >= DHCP_COARSE_TIMER_MSECS) {
-    DHCPcoarseTimer = localtime;
-    dhcp_coarse_tmr();
-  }
-
-#endif
 }
-
-#ifdef USE_DHCP
-/**
-* @brief  LwIP_DHCP_Process_Handle
-* @param  None
-* @retval None
-*/
-void
-LwIP_DHCP_Process_Handle()
-{
-  struct ip_addr ipaddr;
-  struct ip_addr netmask;
-  struct ip_addr gw;
-  uint8_t iptab[4] = { 0 };
-  uint8_t iptxt[20];
-
-  switch (DHCP_state) {
-    case DHCP_START: {
-      DHCP_state = DHCP_WAIT_ADDRESS;
-      dhcp_start(&gnetif);
-      /* IP address should be set to 0
-         every time we want to assign a new DHCP address */
-      IPaddress = 0;
-#ifdef USE_LCD
-      LCD_DisplayStringLine(Line4, (uint8_t*)"     Looking for    ");
-      LCD_DisplayStringLine(Line5, (uint8_t*)"     DHCP server    ");
-      LCD_DisplayStringLine(Line6, (uint8_t*)"     please wait... ");
-#endif
-    } break;
-
-    case DHCP_WAIT_ADDRESS: {
-      /* Read the new IP address */
-      IPaddress = gnetif.ip_addr.addr;
-
-      if (IPaddress != 0) {
-        DHCP_state = DHCP_ADDRESS_ASSIGNED;
-
-        /* Stop DHCP */
-        dhcp_stop(&gnetif);
-
-#ifdef USE_LCD
-        iptab[0] = (uint8_t)(IPaddress >> 24);
-        iptab[1] = (uint8_t)(IPaddress >> 16);
-        iptab[2] = (uint8_t)(IPaddress >> 8);
-        iptab[3] = (uint8_t)(IPaddress);
-
-        sprintf((char*)iptxt, " %d.%d.%d.%d", iptab[3], iptab[2], iptab[1],
-                iptab[0]);
-
-        LCD_ClearLine(Line4);
-        LCD_ClearLine(Line5);
-        LCD_ClearLine(Line6);
-
-        /* Display the IP address */
-        LCD_DisplayStringLine(Line7, (uint8_t*)"IP address assigned ");
-        LCD_DisplayStringLine(Line8, (uint8_t*)"  by a DHCP server  ");
-        LCD_DisplayStringLine(Line9, iptxt);
-#endif
-        STM_EVAL_LEDOn(LED1);
-      } else {
-        /* DHCP timeout */
-        if (gnetif.dhcp->tries > MAX_DHCP_TRIES) {
-          DHCP_state = DHCP_TIMEOUT;
-
-          /* Stop DHCP */
-          dhcp_stop(&gnetif);
-
-          /* Static address used */
-          IP4_ADDR(&ipaddr, IP_ADDR0, IP_ADDR1, IP_ADDR2, IP_ADDR3);
-          IP4_ADDR(&netmask, NETMASK_ADDR0, NETMASK_ADDR1, NETMASK_ADDR2,
-                   NETMASK_ADDR3);
-          IP4_ADDR(&gw, GW_ADDR0, GW_ADDR1, GW_ADDR2, GW_ADDR3);
-          netif_set_addr(&gnetif, &ipaddr, &netmask, &gw);
-
-#ifdef USE_LCD
-          LCD_DisplayStringLine(Line7, (uint8_t*)"    DHCP timeout    ");
-
-          iptab[0] = IP_ADDR3;
-          iptab[1] = IP_ADDR2;
-          iptab[2] = IP_ADDR1;
-          iptab[3] = IP_ADDR0;
-
-          sprintf((char*)iptxt, "  %d.%d.%d.%d", iptab[3], iptab[2], iptab[1],
-                  iptab[0]);
-
-          LCD_ClearLine(Line4);
-          LCD_ClearLine(Line5);
-          LCD_ClearLine(Line6);
-
-          LCD_DisplayStringLine(Line8, (uint8_t*)"  Static IP address   ");
-          LCD_DisplayStringLine(Line9, iptxt);
-#endif
-          STM_EVAL_LEDOn(LED1);
-        }
-      }
-    } break;
-    default:
-      break;
-  }
-}
-#endif
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
