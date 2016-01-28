2f824a9386ed0ebadfe17e2f1eb9deae2c806abb
Hendrik v. Raven <hendrik@consetetur.de>
2016-01-28 09:25:32 +0000

    clang format on copied files

--- include/netconf.h
+++ include/netconf.h
@@ -3,8 +3,8 @@
   * @file    netconf.h
   * @author  MCD Application Team
   * @version V1.1.0
-  * @date    31-July-2013 
-  * @brief   This file contains all the functions prototypes for the netconf.c 
+  * @date    31-July-2013
+  * @brief   This file contains all the functions prototypes for the netconf.c
   *          file.
   ******************************************************************************
   * @attention
@@ -17,8 +17,8 @@
   *
   *        http://www.st.com/software_license_agreement_liberty_v2
   *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   * See the License for the specific language governing permissions and
   * limitations under the License.
@@ -31,17 +31,17 @@
 #define __NETCONF_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
-#define DHCP_START                 1
-#define DHCP_WAIT_ADDRESS          2
-#define DHCP_ADDRESS_ASSIGNED      3
-#define DHCP_TIMEOUT               4
-#define DHCP_LINK_DOWN             5
+#define DHCP_START 1
+#define DHCP_WAIT_ADDRESS 2
+#define DHCP_ADDRESS_ASSIGNED 3
+#define DHCP_TIMEOUT 4
+#define DHCP_LINK_DOWN 5
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 void LwIP_Init(void);
@@ -53,6 +53,5 @@ void LwIP_Periodic_Handle(__IO uint32_t localtime);
 #endif
 
 #endif /* __NETCONF_H */
-
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- include/stm32f4x7_eth_bsp.h
+++ include/stm32f4x7_eth_bsp.h
@@ -3,7 +3,7 @@
   * @file    stm32f4x7_eth_bsp.h
   * @author  MCD Application Team
   * @version V1.1.0
-  * @date    31-July-2013 
+  * @date    31-July-2013
   * @brief   Header for stm32f4x7_eth_bsp.c file.
   ******************************************************************************
   * @attention
@@ -16,8 +16,8 @@
   *
   *        http://www.st.com/software_license_agreement_liberty_v2
   *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   * See the License for the specific language governing permissions and
   * limitations under the License.
@@ -30,7 +30,7 @@
 #define __STM32F4x7_ETH_BSP_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
@@ -38,35 +38,34 @@
 #include "lwip/netif.h"
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
-#define DP83848_PHY_ADDRESS       0x01 /* Relative to STM324xG-EVAL Board */
+#define DP83848_PHY_ADDRESS 0x01 /* Relative to STM324xG-EVAL Board */
 
 /* Specific defines for EXTI line, used to manage Ethernet link status */
-#define ETH_LINK_EXTI_LINE             EXTI_Line14
-#define ETH_LINK_EXTI_PORT_SOURCE      EXTI_PortSourceGPIOB
-#define ETH_LINK_EXTI_PIN_SOURCE       EXTI_PinSource14
-#define ETH_LINK_EXTI_IRQn             EXTI15_10_IRQn 
+#define ETH_LINK_EXTI_LINE EXTI_Line14
+#define ETH_LINK_EXTI_PORT_SOURCE EXTI_PortSourceGPIOB
+#define ETH_LINK_EXTI_PIN_SOURCE EXTI_PinSource14
+#define ETH_LINK_EXTI_IRQn EXTI15_10_IRQn
 /* PB14 */
-#define ETH_LINK_PIN                   GPIO_Pin_14
-#define ETH_LINK_GPIO_PORT             GPIOB
-#define ETH_LINK_GPIO_CLK              RCC_AHB1Periph_GPIOB
+#define ETH_LINK_PIN GPIO_Pin_14
+#define ETH_LINK_GPIO_PORT GPIOB
+#define ETH_LINK_GPIO_CLK RCC_AHB1Periph_GPIOB
 
 /* Ethernet Flags for EthStatus variable */
-#define ETH_INIT_FLAG           0x01 /* Ethernet Init Flag */
-#define ETH_LINK_FLAG           0x10 /* Ethernet Link Flag */
+#define ETH_INIT_FLAG 0x01 /* Ethernet Init Flag */
+#define ETH_LINK_FLAG 0x10 /* Ethernet Link Flag */
 
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-void  ETH_BSP_Config(void);
+void ETH_BSP_Config(void);
 uint32_t Eth_Link_PHYITConfig(uint16_t PHYAddress);
 void Eth_Link_EXTIConfig(void);
 void Eth_Link_ITHandler(uint16_t PHYAddress);
-void ETH_link_callback(struct netif *netif);
+void ETH_link_callback(struct netif* netif);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __STM32F4x7_ETH_BSP_H */
-
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
