869e8af55b2dbdebeb7cafe59e742e60d35e8b0c
Hendrik v. Raven <hendrik@consetetur.de>
2016-02-02 14:05:40 +0000

    clean up main.c

--- src/main.c
+++ src/main.c
@@ -25,22 +25,8 @@
 #include "spi.h"
 #include "gpio.h"
 
-/** @addtogroup STM32F4_Discovery_Peripheral_Examples
-  * @{
-  */
 
-/** @addtogroup IO_Toggle
-  * @{
-  */
 
-/* Private typedef -----------------------------------------------------------*/
-GPIO_InitTypeDef GPIO_InitStructure;
-
-/**
-  * @brief  Main program
-  * @param  None
-  * @retval None
-  */
 int
 main(void)
 {
@@ -70,32 +56,15 @@ main(void)
 
 #ifdef USE_FULL_ASSERT
 
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void
 assert_failed(uint8_t* file, uint32_t line)
 {
   /* User can add his own implementation to report the file name and line
      number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-
   gpio_blink_forever_fast(LED_RED);
 }
 #endif
 
-/**
-  * @}
-  */
 
-/**
-  * @}
-  */
 
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
