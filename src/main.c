73bbc41a7bbd5d42286a2455e6a562ead056a68f
Hendrik v. Raven <hendrik@consetetur.de>
2016-02-04 12:21:43 +0000

    cleanup

--- src/main.c
+++ src/main.c
@@ -1,25 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    IO_Toggle/main.c
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    19-September-2011
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "ad9910.h"
 #include "gpio.h"
 #include "ethernet.h"
@@ -48,8 +26,5 @@ main(void)
 void
 assert_failed(uint8_t* file, uint32_t line)
 {
-  /* User can add his own implementation to report the file name and line
-     number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   gpio_blink_forever_fast(LED_RED);
 }
