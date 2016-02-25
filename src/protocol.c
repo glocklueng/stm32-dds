a5c05f954c79f30e6892790103461ebe5f303517
Hendrik v. Raven <hendrik@consetetur.de>
2016-02-25 11:25:05 +0000

    rewrote handlers to use begin and end ptr
    we now use pointers to the first and behind the last byte instead of a
    pointer to the first byte and a length

--- src/protocol.c
+++ src/protocol.c
@@ -29,25 +29,38 @@ static struct protocol_state ps;
 struct protocol_handler
 {
   const char* id;
-  const char* (*parser)(struct protocol_state*, const char*, size_t);
+  const char* (*parser)(struct protocol_state*, const char*, const char*);
 };
 
 static void align_buffer(char* buffer, char** begin, char** end);
 static void protocol_assemble_packet(struct protocol_state*, struct pbuf*);
-static int protocol_data_transfer(struct protocol_state*, struct pbuf*);
-static int protocol_switch_packet(struct protocol_state*, const char*, size_t);
+static const char* protocol_data_transfer(struct protocol_state*, struct pbuf*);
+static const char* protocol_switch_packet(struct protocol_state*, const char*,
+                                          const char*);
 
 static const char* generic_switch_packet(struct protocol_handler*,
                                          struct protocol_state*, const char*,
-                                         size_t);
+                                         const char*);
 static const char* output_subsystem_handler(struct protocol_state*, const char*,
-                                            size_t);
+                                            const char*);
 static const char* output_freq_handler(struct protocol_state*, const char*,
-                                       size_t);
+                                       const char*);
 
-static void skip_whitespace(const char*, size_t, char**);
-static uint32_t parse_frequency(const char*, size_t, char**);
-static const char* skip_till_end_of_line(const char*, size_t);
+static inline size_t distance(const void*, const void*);
+
+static void skip_whitespace(const char**, const char*);
+static void skip_till_end_of_line(const char**, const char*);
+static uint32_t parse_frequency(const char**, const char*);
+static uint16_t parse_amplitude(const char**, const char*);
+
+static inline size_t
+distance(const void* fst, const void* snd)
+{
+  if (fst < snd)
+    return snd - fst;
+  else
+    return fst - snd;
+}
 
 struct protocol_state*
 protocol_accept_connection(struct server_state* es)
@@ -122,194 +135,176 @@ protocol_assemble_packet(struct protocol_state* ps, struct pbuf* p)
 {
   /* we use a static memory array for intermediate data storage */
   static char buffer[INPUT_BUFFER_SIZE];
-  static char* begin = buffer;
-  static char* end = buffer;
+  static char* buffer_begin = buffer;
+  static char* buffer_end = buffer;
 
-  size_t used = 0;
+  const char* data_begin = p->payload;
+  const char* data_end = p->payload + p->len;
 
   /* if we are in a data transfer phase we don't parse the contents.
    * Being in that phase implies that the buffer is empty, but the
    * function may not consume all bytes.
    */
   if (ps->status == data_transfer) {
-    used += protocol_data_transfer(ps, p);
+    data_begin = protocol_data_transfer(ps, p);
   }
 
   /* if the buffer was empty we don't have to copy anything */
-  if (begin == end) {
+  if (buffer_begin == buffer_end) {
     /* no previous data */
 
-    while (used < p->len) {
-      const size_t pused = used;
-      const size_t remaining = p->len - used;
-      used += protocol_switch_packet(ps, p->payload + used, remaining);
+    while (data_begin < data_end) {
+      const char* temp_begin = data_begin;
+      data_begin = protocol_switch_packet(ps, data_begin, data_end);
 
       /* if the last call didn't consume any data we stop and save the
        * remaining bytes */
-      if (pused == used) {
-        memcpy(begin, p->payload + used, remaining);
-        end = begin + remaining;
-        align_buffer(buffer, &begin, &end);
+      if (temp_begin == data_begin) {
+        memcpy(buffer_begin, data_begin, distance(data_begin, data_end));
+        buffer_end = buffer_begin + distance(data_begin, data_end);
+        align_buffer(buffer, &buffer_begin, &buffer_end);
         return;
       }
     }
   } else {
     /* we do have data in the buffer and are not in the transfer phase */
 
-    /* these keep track of the already copied data */
-    char* payload = p->payload;
-    size_t len = p->len;
-
     /* as long as not all data is copied we copy as much as possible */
-    while (len != 0) {
-      const size_t buf_remaining = INPUT_BUFFER_SIZE - (end - buffer);
-      if (len < buf_remaining) {
+    while (data_begin < data_end) {
+      const size_t buf_remaining = INPUT_BUFFER_SIZE - (buffer_end - buffer);
+      if (distance(data_begin, data_end) < buf_remaining) {
         /* whole package does fit in remaining buffer */
-        memcpy(end, payload, len);
-        end += len;
-        len = 0;
+        memcpy(buffer_end, data_begin, distance(data_begin, data_end));
+        buffer_end += distance(data_begin, data_end);
+        data_begin = data_end;
       } else {
         /* only parts of the buffer do fit */
-        memcpy(end, payload, buf_remaining);
-        end += buf_remaining;
-        payload += buf_remaining;
-        len -= buf_remaining;
+        memcpy(buffer_end, data_begin, buf_remaining);
+        buffer_end += buf_remaining;
+        data_begin += buf_remaining;
       }
 
       /* keep track of the start of the last round */
       char* last = NULL;
 
-      const size_t pused = used;
-
       /* parse as much of the data in the buffer as we can */
-      while (begin < end && last != begin) {
-        last = begin;
-        size_t tused = protocol_switch_packet(ps, begin, end - begin);
-        used += tused;
-        begin += tused;
+      while (buffer_begin < buffer_end && last != buffer_begin) {
+        last = buffer_begin;
+        buffer_begin =
+          (char*)protocol_switch_packet(ps, buffer_begin, buffer_end);
       }
 
       /* if we didn't parse anything and the buffer is full we abort. This
        * means we dump all saved data and output an error message */
-      if (used == pused && (end - begin) == INPUT_BUFFER_SIZE) {
-        begin = buffer;
-        end = begin;
+      if (buffer_begin == last && buffer_end == buffer + INPUT_BUFFER_SIZE) {
+        buffer_begin = buffer;
+        buffer_end = buffer_begin;
 
         ethernet_queue(ps->es, "protocol error: ethernet buffer full\n", 37);
         return;
       }
 
       /* move contents to buffer front before we copy the next data */
-      align_buffer(buffer, &begin, &end);
+      align_buffer(buffer, &buffer_begin, &buffer_end);
     }
   }
 }
 
-static int
+static const char*
 protocol_data_transfer(struct protocol_state* es, struct pbuf* p)
 {
-  return 0;
+  return p->payload;
 }
 
 static const char*
 generic_switch_packet(struct protocol_handler* handler,
-                      struct protocol_state* es, const char* data, size_t len)
+                      struct protocol_state* es, const char* begin,
+                      const char* end)
 {
   for (; handler->parser != NULL; handler++) {
     const size_t slen = strlen(handler->id);
 
-    if (slen < len && strncmp(data, handler->id, slen) == 0) {
-      const int header_len = strlen(handler->id);
-      return handler->parser(es, data + header_len, len - header_len);
+    if (slen < distance(begin, end) && strncmp(begin, handler->id, slen) == 0) {
+      return handler->parser(es, begin + slen, end);
     }
   }
 
   return NULL;
 }
 
-static int
-protocol_switch_packet(struct protocol_state* es, const char* data, size_t len)
+static const char*
+protocol_switch_packet(struct protocol_state* es, const char* begin,
+                       const char* end)
 {
   static struct protocol_handler subsystem_handler_list[] = {
     { "OUTPUT:", output_subsystem_handler }, { NULL, NULL }
   };
 
-  const char* end = data;
-
-  while (end && end < data + len) {
-    end = generic_switch_packet(subsystem_handler_list, es, data, len);
-    if (end == NULL) {
-      break;
-    }
-    end = skip_till_end_of_line(end, len - (end - data));
+  while (begin < end) {
+    begin = generic_switch_packet(subsystem_handler_list, es, begin, end);
+    skip_till_end_of_line(&begin, end);
   }
 
-  return end - data;
+  return begin;
 }
 
 static const char*
-output_subsystem_handler(struct protocol_state* es, const char* data,
-                         size_t len)
+output_subsystem_handler(struct protocol_state* es, const char* begin,
+                         const char* end)
 {
   static struct protocol_handler output_subsystem_handler_list[] = {
     { "FREQ", output_freq_handler }, { NULL, NULL }
   };
 
-  return generic_switch_packet(output_subsystem_handler_list, es, data, len);
+  return generic_switch_packet(output_subsystem_handler_list, es, begin, end);
 }
 
 static const char*
-output_freq_handler(struct protocol_state* es, const char* data, size_t len)
+output_freq_handler(struct protocol_state* es, const char* begin,
+                    const char* end)
 {
-  char* endptr;
-  uint32_t freq = parse_frequency(data, len, &endptr);
+  uint32_t freq = parse_frequency(&begin, end);
 
   ad9910_set_frequency(0, freq);
   ad9910_io_update();
 
-  return endptr;
+  return begin;
 }
 
 static uint32_t
-parse_frequency(const char* data, size_t len, char** end)
+parse_frequency(const char** pbegin, const char* end)
 {
-  double freq = strtod(data, end);
+  double freq = strtod(*pbegin, (char**)pbegin);
 
-  char* tend = *end;
+  skip_whitespace(pbegin, end);
 
-  skip_whitespace(data, len, end);
-
-  size_t used = *end - data;
-  if (len - used >= 2 && strncasecmp("Hz", *end, 2) == 0) {
-    *end += 2;
-  } else {
-    *end = tend;
+  if (distance(*pbegin, end) >= 2 && strncasecmp("Hz", *pbegin, 2) == 0) {
+    *pbegin += 2;
   }
 
   return ad9910_convert_frequency(freq);
 }
 
 static void
-skip_whitespace(const char* data, size_t len, char** end)
+skip_whitespace(const char** pbegin, const char* end)
 {
-  *end = (char*)data;
-  for (; len > 0; --len) {
-    if (!isspace(**end)) {
+  while (*pbegin < end) {
+    if (!isspace(**pbegin)) {
       return;
     }
 
-    *end += 1;
+    *pbegin += 1;
   }
 }
 
-static const char*
-skip_till_end_of_line(const char* data, size_t len)
+static void
+skip_till_end_of_line(const char** pbegin, const char* end)
 {
-  for (size_t i = 0; i < len; ++i) {
-    if (data[i] == '\n') {
-      return data + i + 1;
+  while (*pbegin < end) {
+    if (**pbegin == '\n') {
+      return;
     }
-  }
 
-  return NULL;
+    *pbegin += 1;
+  }
 }
