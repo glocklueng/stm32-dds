8e853f6400224afe9e82774da1674825c56dc619
Hendrik v. Raven <hendrik@consetetur.de>
2016-04-23 01:05:13 +0100

    keep track of current command state

--- src/commands.c
+++ src/commands.c
@@ -20,6 +20,8 @@ static void wait_for_trigger(ad9910_trigger_mode);
 
 static int any_output_is(ad9910_command_type);
 
+static void update_state(ad9910_target, ad9910_command_type);
+
 static ad9910_command command_state = {
   .trigger = ad9910_trigger_none,
   .frequency = 0,
@@ -94,24 +96,30 @@ get_full_command_size(ad9910_command* cmd)
 static uint32_t
 prepare_command(ad9910_target tgt, ad9910_command_type cmd, const void* data)
 {
+  uint32_t ret;
+
   switch (cmd) {
     /* this case shouldn't happen, but just in case we stop execution */
     default:
     case ad9910_command_none:
-      return 0;
+      ret = 0;
     case ad9910_command_fixed:
       prepare_fixed_command(tgt, data);
-      return sizeof(ad9910_fixed_command);
+      ret = sizeof(ad9910_fixed_command);
     case ad9910_command_parallel:
       prepare_parallel_command(tgt, data);
-      return sizeof(ad9910_parallel_command);
+      ret = sizeof(ad9910_parallel_command);
     case ad9910_command_ram:
       prepare_ram_command(tgt, data);
-      return sizeof(ad9910_ram_command);
+      ret = sizeof(ad9910_ram_command);
     case ad9910_command_ramp:
       prepare_ramp_command(tgt, data);
-      return sizeof(ad9910_ramp_command);
+      ret = sizeof(ad9910_ramp_command);
   }
+
+  update_state(tgt, cmd);
+
+  return ret;
 }
 
 static void
@@ -198,4 +206,21 @@ any_output_is(ad9910_command_type type)
   return (command_state.frequency | command_state.amplitude |
           command_state.phase) &
          type;
+}
+
+static void
+update_state(ad9910_target tgt, ad9910_command_type cmd)
+{
+  if (cmd == ad9910_command_none) {
+    return;
+  }
+
+  switch (tgt) {
+    case ad9910_frequency:
+      command_state.frequency = cmd;
+    case ad9910_amplitude:
+      command_state.amplitude = cmd;
+    case ad9910_phase:
+      command_state.phase = cmd;
+  }
 }
